d9c67ebcdc2e2b8b2345bd74568e23089e333694
Anthony Sandhar-Cruz <123418080+AnthonySandhar-Cruz@users.noreply.github.com>
2024-03-17 14:40:33 -0400

    Initial commit

---
+++ calories.py
@@ -0,0 +1,93 @@
+import numpy as np
+import sys
+import os
+import shutil
+from datetime import date
+
+
+def tbsp_to_cups(tbsp):
+    return tbsp*0.0625
+
+
+def tsp_to_cups(tsp):
+    return tsp*0.0208
+
+
+def ml_to_cups(ml):
+    return ml*0.00423
+
+
+def l_to_cups(l):
+    return l*4.227
+
+
+def find_total():
+    try:
+        food, quantity, units = np.loadtxt('calories.txt', skiprows=2, unpack=True, delimiter=',', dtype=str)
+        total = 0
+        for i in range(len(food)):
+            quant = float(quantity[i])
+            if food[i].lower() in FOODS and units[i].lower() in UNITS:
+                match units[i].lower():
+                    case 'cups':
+                        total += FOODS[food[i]]*quant
+                    case 'none':
+                        total += FOODS[food[i]]*quant
+                    case 'ml':
+                        quantity = ml_to_cups(quant)
+                        total += FOODS[food[i]]*quant
+                    case 'l':
+                        quantity = l_to_cups(quant)
+                        total += FOODS[food[i]]*quant
+                    case 'tsp':
+                        quantity = tsp_to_cups(quant)
+                        total += FOODS[food[i]]*quant
+                    case 'tbsp':
+                        quantity = tbsp_to_cups(quant)
+                        total += FOODS[food[i]]*quant
+            else:
+                print('Invalid entry in calories file. Check file and retry.')
+        return total
+    except:
+        print('Error finding total calories.')
+
+
+FOODS = {'banana':100, 'kiwi':50}
+UNITS = ['ml', 'l', 'tsp', 'tbsp', 'cups', 'none']
+
+if sys.argv[1] == 'clear' or sys.argv[1] == 'init':
+    try:
+        with open('calories.txt', 'w') as calories:
+            calories.write(f'Date: {date.today()}\n')
+            calories.write('Food,Quantity,Units\n')
+    except:
+        print('Error clearing calories file.')
+elif sys.argv[1] == 'dump':
+    try:
+        with open('calories.txt', 'a') as calories:
+            calories.write(f'Total Calories:\n{find_total()}')
+        source = 'calories.txt'
+        destination = f'{date.today()}.txt'
+        shutil.copy(source, destination)
+    except:
+        print('Error dumping contents of calories file.')
+elif sys.argv[1] == 'add':
+    try:
+        if sys.argv[2].lower() in FOODS: 
+            with open('calories.txt', 'a') as calories:
+                calories.write(f'{sys.argv[2]},{sys.argv[3]},{sys.argv[4]}\n')
+        else:
+            print('Please add food to dictionary.')
+    except:
+        print('Error adding food to calories file.')
+elif sys.argv[1] == 'total':
+    print(find_total())
+elif sys.argv[1] == 'print':
+    try:
+        with open('calories.txt', 'r') as calories:
+            print(calories.read())
+            print(f'Total calories: {find_total()}')
+    except:
+        print('Error printing contents of calories file.')
+else:
+    print('Invalid command.')
